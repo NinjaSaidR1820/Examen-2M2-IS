3d617ea947a1ee89acb4f0af35e48aaf277c9614
NinjaSaidR1820 <saidrivas1820@hotmail.com>
2022-05-04 09:45:45 -0600

    Services

---
+++ Examen 2M2-IS/AppCore/Services/BaseServices.cs
@@ -0,0 +1,24 @@
+﻿using AppCore.IService;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCore.Services
+{
+    public class BaseServices<T> : IServices<T>
+    {
+        private IModel<T> Model;
+
+        protected BaseServices(IModel<T> model)
+        {
+            this.Model = model;
+        }
+        public void Add(T t)
+        {
+            Model.Add(t);
+        }
+    }
+}
